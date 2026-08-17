a60aecd8885b4abf382feb8c203b28473c9c32ec
dipak1920basnet <dipak1920basnet@gmail.com>
2026-08-17 19:55:43 +0545

    set a class blueprint for usesr

---
+++ user.py
@@ -0,0 +1,36 @@
+from expense import Expenses
+
+class User:
+    def __init__(self,name:str=None, password:str=None, email:str=None):
+        self.name = name
+        self.email = email
+        self.password = password
+        self.Expense = []
+
+    def logged_in():
+        return False
+
+    def confirm_password(self, password:str)->bool:
+        if self.password == password:
+            return True
+        return False
+
+    def confirm_email(self, email:str) -> bool:
+        if self.email == email:
+            return True
+        return False
+    
+    def change_password(self, old_password:str, new_password:str, confirm_password:str)->None:
+        if self.confirm_password(old_password) and new_password == confirm_password:
+            self.password = new_password
+
+    def change_name(self, email:str, password:str, new_name:str)->None:
+        if (self.confirm_email(email) and self.confirm_password(password)) or self.loged_in():
+            self.name = new_name
+
+    def add_Expense(self, expense:Expenses)->None:
+        if isinstance(self.Expense) != list:
+            self.Expense = []
+        self.Expense.append(expense)
+
+    
